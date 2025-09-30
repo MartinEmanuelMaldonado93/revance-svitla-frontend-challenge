9c9fc048dd0891304fc6c01af1d1730137758606
martin emanuel <martin.maldonado01@globant.com>
2025-09-30 16:48:27 -0300

    styles using tw on challenge 2

--- 2. Gradient background logo/gradient-bg-logo/src/App.svelte
+++ 2. Gradient background logo/gradient-bg-logo/src/App.svelte
@@ -1,47 +1,18 @@
-<script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
-</script>
-
-<main>
-  <div>
-    <a href="https://vite.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
+<div
+	class="w-[220px] h-[220px] bg-white/90 backdrop-blur-xl border border-white/30 shadow-lg rounded-[4rem] flex flex-col justify-center items-center p-5 box-border"
+>
+	<svg
+		class="w-20 h-20 fill-black/80 mb-4"
+		xmlns:xlink="http://www.w3.org/1999/xlink"
+		xmlns="http://www.w3.org/2000/svg"
+		fill="#000000"
+		width="96"
+		height="96"
+		viewBox="0 0 32 32"
+	>
+		<path
+			d="M4.807 0l6.391 2.25v22.495l9.005-5.193-4.411-2.073-2.786-6.932 14.188 4.984v7.245l-15.99 9.224-6.396-3.563z"
+		/>
+	</svg>
+	<p class="m-0 text-xl font-extralight text-black/80">Bing</p>
+</div>
