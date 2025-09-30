7124cf0142ce74f70a2ce476f3552fee43948ec8
martin emanuel <martin.maldonado01@globant.com>
2025-09-30 16:26:43 -0300

    done challenge 1 on svelte

--- 1. Navigation hover/navigation-hover-svelte/src/App.svelte
+++ 1. Navigation hover/navigation-hover-svelte/src/App.svelte
@@ -1,12 +1,30 @@
-<script lang="ts">
-</script>
-
-<main>
-	<nav>
-		<ul class="flex gap-4">
-			<li><a href="/">Animation</a></li>
-			<li><a href="/">Branding</a></li>
-			<li><a href="/">Illustration</a></li>
-		</ul>
-	</nav>
-</main>
+<nav
+	class="flex justify-center items-center font-poppins w-full min-h-screen m-0 bg-gray-800 bg-gradient-to-b from-[hsl(223,78%,4%)] from-50% to-[hsl(218,45%,19%)] to-100%"
+>
+	<ul class="list-none m-0 p-0 text-center select-none md:flex md:flex-row">
+		<li class="m-4 md:m-0 md:mx-5">
+			<a
+				href="/"
+				class="text-decoration-none text-gray-400 text-4xl font-medium relative py-3 transition-colors duration-300 ease-in-out hover:text-white after:content-[''] after:absolute after:bottom-0 after:left-1/2 after:w-12 after:h-0.5 after:bg-white after:rounded-sm after:transform-origin-left after:scale-x-0 after:-translate-x-1/2 after:transition-transform after:duration-1000 after:ease-[cubic-bezier(0.175,0.885,0.32,1)] hover:after:scale-x-100 active:after:scale-x-100 md:text-2xl"
+			>
+				Animation
+			</a>
+		</li>
+		<li class="m-4 md:m-0 md:mx-5">
+			<a
+				href="/"
+				class="text-decoration-none text-gray-400 text-4xl font-medium relative py-3 transition-colors duration-300 ease-in-out hover:text-white after:content-[''] after:absolute after:bottom-0 after:left-1/2 after:w-12 after:h-0.5 after:bg-white after:rounded-sm after:transform-origin-left after:scale-x-0 after:-translate-x-1/2 after:transition-transform after:duration-1000 after:ease-[cubic-bezier(0.175,0.885,0.32,1)] hover:after:scale-x-100 active:after:scale-x-100 md:text-2xl"
+			>
+				Branding
+			</a>
+		</li>
+		<li class="m-4 md:m-0 md:mx-5">
+			<a
+				href="/"
+				class="text-decoration-none text-gray-400 text-4xl font-medium relative py-3 transition-colors duration-300 ease-in-out hover:text-white after:content-[''] after:absolute after:bottom-0 after:left-1/2 after:w-12 after:h-0.5 after:bg-white after:rounded-sm after:transform-origin-left after:scale-x-0 after:-translate-x-1/2 after:transition-transform after:duration-1000 after:ease-[cubic-bezier(0.175,0.885,0.32,1)] hover:after:scale-x-100 active:after:scale-x-100 md:text-2xl"
+			>
+				Illustration
+			</a>
+		</li>
+	</ul>
+</nav>
