73fa0c772c1a332df4beaf2fcec481d7623ac34d
martin emanuel <martin.maldonado01@globant.com>
2025-09-30 15:55:33 -0300

    add svelte package setup project

---
+++ 1. Navigation hover/navigation-hover-svelte/src/App.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+</script>
+
+<main>
+	<nav>
+		<ul class="flex gap-4">
+			<li><a href="/">Animation</a></li>
+			<li><a href="/">Branding</a></li>
+			<li><a href="/">Illustration</a></li>
+		</ul>
+	</nav>
+</main>
